26d7f6f0a22c05e9f3a297e77c687fa8971477a9
Saiful Alam <88210205+saiful1604008@users.noreply.github.com>
2022-09-07 17:03:31 +0600

    Time: 930 ms (53.26%), Space: 0B (100.00%) - LeetHub

---
+++ 196-delete-duplicate-emails/196-delete-duplicate-emails.sql
@@ -0,0 +1,6 @@
+# Please write a DELETE statement and DO NOT write a SELECT statement.
+# Write your MySQL query statement below
+
+delete p1 from person as p1
+INNER JOIN person as p2 
+where p1.id > p2.id and p1.email = p2.email;
